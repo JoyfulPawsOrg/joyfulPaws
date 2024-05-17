fa4341c91f4b183f91b71591f0fceab6bc9f6d18
Lamis <lemees5510@gmail.com>
2024-05-17 18:25:14 +0300

    modify

--- Emergency.php
+++ Emergency.php
@@ -645,6 +645,7 @@ top: 13px;
   <script src="js/script.js"></script>
   <script src="js/iconify.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  
 </body>
 
 </html>
